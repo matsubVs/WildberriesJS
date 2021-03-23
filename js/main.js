4bf05318ef2fb257afb4cfca8e74402b2d9232da
Danila Sokolov <matsubus@mail.ru>
2021-03-23 19:41:11 +0300

    add gitignore

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/*global require*/
+/*eslint-env es6*/
+
 const mySwiper = new Swiper('.swiper-container', {
 	loop: true,
 
@@ -8,3 +11,41 @@ const mySwiper = new Swiper('.swiper-container', {
 	},
 });
 
+// cart
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+const overlay = document.querySelector('.overlay');
+
+const openModal = () => {
+	modalCart.classList.add('show');
+}
+
+const closeModal = event => {
+	const t = event.target;
+
+	if (!t.closest('.modal')) {
+		modalCart.classList.remove('show');
+	} else if (t.matches('.modal-close')) {
+		modalCart.classList.remove('show');
+	}
+}
+
+overlay.addEventListener('click', closeModal);
+buttonCart.addEventListener('click', openModal);
+
+//scroll smooth 
+{
+	const scrollLink = document.querySelectorAll('a.scroll-link');
+
+	for (let i = 0; i < scrollLink.length; i++) {
+		scrollLink[i].addEventListener('click', event => {
+			event.preventDefault();
+			const id = scrollLink[i].getAttribute('href');
+	
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			});
+		});
+	}
+}
